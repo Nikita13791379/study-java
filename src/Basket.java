426ba1722cf10c18404ea4a88c1b208bffd04b14
RobotComp.ru <Nikita1379Nikita137@mail.ru>
2024-06-21 13:58:23 +0500

    commit 21.06.2024 13:58

---
+++ src/Basket.java
@@ -0,0 +1,40 @@
+import javax.xml.namespace.QName;
+
+public class Basket {
+    private  String items = "";
+    private  int totalPrice = 0;
+
+
+
+
+    public  void add(String name, int price) {
+        if (contains(name)) {
+            return;
+        }
+        items = items + "\n" + name + "-" + price;
+        totalPrice = totalPrice + price;
+    }
+
+    public  void clear() {
+        items = "";
+        totalPrice = 0;
+    }
+
+    public  int getTotalPrice() {
+        return totalPrice;
+    }
+
+    public  boolean contains(String name) {
+        return (items.contains(name));
+
+        }
+
+        public  void print (String title){
+            System.out.println(title);
+            if (items.isEmpty()) {
+                System.out.println("корзина пуста");
+            } else {
+                System.out.println(items);
+            }
+        }
+    }
